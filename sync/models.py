63956151962a7131b8406da0d0a8a4a1f1d2d5d5
George Papagapitos <georgepapagapitos@Georges-MacBook-Pro.local>
2024-09-20 20:23:43 -0500

    rm imdb rating

--- sync/models.py
+++ sync/models.py
@@ -6,13 +6,11 @@ class Movie(models.Model):
     summary = models.TextField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     duration = models.IntegerField(null=True, blank=True)
-    rating = models.FloatField(null=True, blank=True)
     poster_url = models.URLField(null=True, blank=True)
     genres = models.TextField(null=True, blank=True)
     plex_key = models.CharField(max_length=255, unique=True)
     trailer_url = models.URLField(null=True, blank=True)
     tmdb_id = models.PositiveIntegerField(null=True, blank=True)
-    imdb_rating = models.FloatField(null=True, blank=True)
     rotten_tomatoes_rating = models.FloatField(null=True, blank=True)
 
     def __str__(self):
