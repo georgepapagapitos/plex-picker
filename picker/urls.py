64847b1542432c3b0d80dc16ff46fb3d2baf1c38
georgepapagapitos <63572045+georgepapagapitos@users.noreply.github.com>
2024-09-22 19:04:29 -0500

    change content to home (#19)
    * change content to home
    
    * testing

--- picker/urls.py
+++ picker/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from picker.views import fetch_plex_content, fetch_random_movie, movie_detail_view
 
 urlpatterns = [
-    path("content/", fetch_plex_content, name="fetch_plex_content"),
+    path("", fetch_plex_content, name="fetch_plex_content"),
     path("random-movie/", fetch_random_movie, name="fetch_random_movie"),
     path("movies/<int:movie_id>/", movie_detail_view, name="movie_detail"),
 ]
